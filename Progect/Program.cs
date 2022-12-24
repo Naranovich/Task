69906031205b9417e053bcfc806b1399dbed2f2f
Naranovich <nik.naranovich@mail.ru>
2022-12-24 16:18:29 +0300

    методы

--- Progect/Program.cs
+++ Progect/Program.cs
@@ -1,3 +1,25 @@
 ﻿string[] arr1 = new string[4] {"2", "hello", "world", ":)"};
 string[] arr2 = new string[arr1.Length];
 int count = 0;
+void SecondArrayWithIF(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arr1, arr2);
+PrintArray(arr2);
